890257bd51a23b61bb87605abce3d7cdb6e914da
Elena Saygo <elena.saygo@gmail.com>
2016-01-06 13:52:23 -0800

    index.html updated, README updates

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # guessing-game
 Guessing game
+1/06/15 Game updated with a counter for the correct answers. Introduced a random number guessing question.
--- app.js
+++ app.js
@@ -1,40 +1,69 @@
 
+var counter = 0;
+var counterNo = 0;
+var randomNumber;
+var userGuess;
+
+
   var user = prompt ('What is your name?');
   alert ('Welcome, ' + user + ' I will ask you some yes/no questions, please answer with Y or N');
   confirm('Are you ready for the game?');
-  console.log('The user\'s name is: ' + user);
 
   var answer1 = prompt('Is it wrong for a vegetarian to eat animal crackers?');
-  console.log('The user answered question1 with ' + answer1);
-  if ( answer1 === 'N'){
+
+  // make it so that we accept 'n' or 'N' or 'no' or 'NO' as an acceptable response
+  if (answer1.toUpperCase() === 'N' || answer1.toUpperCase() === 'NO'){
     alert('You are correct');
+    counter ++;
   }
   else {
     alert('I guess you are wrong. Here comes the next question');
   }
 
   var answer2 = prompt("Is it ok to shoot tourists during 'tourist season'?");
-  console.log('The user answered question2 with ' + answer2);
-  if ( answer2 === 'N'){
+
+  if ( answer2.toUpperCase() === 'N'|| answer2.toUpperCase() === 'NO'){
     alert('You are correct');
+    counter ++;
   }
   else {
     alert('I guess you are wrong. Here comes the next question');
   }
 
   var answer3 = prompt('Do you like to sing in the shower?');
-  console.log('The user answered question3 with ' + answer3);
-  if ( answer3 === 'Y'){
+
+  if ( answer3.toUpperCase() === 'Y' || answer3.toUpperCase() === 'YES'){
     alert('Good for you!');
+    counter ++;
   }
   else {
     alert('Maybe you should consider singing in the shower!');
   }
 
-  if (answer1==="N" & answer2 === "N" & answer3 === "Y") {
-    document.write('Congratulations, you are a genius!');
+  if (counter >= 2) {
+    document.write(user + ', congratulations, you got ' + counter + ' correct answers!');
+}
+  else if (counter = 1) {
+    document.write(user + ', you got only ' + counter + ' correct answers.');
   }
   else {
-
-    document.write('You are not a genius, but thank you for playing the game, '+ user);
+    document.write(user + ', sorry, you got ' + counter + ' correct answers.');
   }
+
+userGuess = prompt (user + ', I hope you are up for a number guessing game :). I am thinking of a number between 1 and 10, what is it?');
+
+//Generate a random number between 1 and 10
+randomNumber = Math.floor((Math.random() * 10) + 1);
+
+while (userGuess != randomNumber)  {
+  if (userGuess > randomNumber) {
+    userGuess = prompt ('Your number is too high.Keep guessing!');
+    counterNo++;
+  }
+  else if (userGuess <randomNumber) {
+    userGuess = prompt ('Your number is too low.Keep guessing!');
+    counterNo++;
+  }
+}
+
+document.write('<p>'+ user + ' ,you guessed in ' + counterNo + ' tries. The correct number is' +randomNumber + '</p>');
