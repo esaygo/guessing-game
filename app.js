6b6a23d587f0838100d03b0c54e7c3ce0965d5b5
Greg Magdsick <greg.magdsick@gmail.com>
2016-01-07 13:34:17 -0800

    Moved questions into functions

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var userGuess;
   var user = prompt ('What is your name?');
   alert ('Welcome, ' + user + ' I will ask you some yes/no questions, please answer with Y or N');
   confirm('Are you ready for the game?');
-
+function  q1() {
   var answer1 = prompt('Is it wrong for a vegetarian to eat animal crackers?');
 
   // make it so that we accept 'n' or 'N' or 'no' or 'NO' as an acceptable response
@@ -20,7 +20,9 @@ var userGuess;
   else {
     alert('I guess you are wrong. Here comes the next question');
   }
+}
 
+function q2() {
   var answer2 = prompt("Is it ok to shoot tourists during 'tourist season'?");
 
   if ( answer2.toUpperCase() === 'N'|| answer2.toUpperCase() === 'NO'){
@@ -30,7 +32,9 @@ var userGuess;
   else {
     alert('I guess you are wrong. Here comes the next question');
   }
+}
 
+function q3() {
   var answer3 = prompt('Do you like to sing in the shower?');
 
   if ( answer3.toUpperCase() === 'Y' || answer3.toUpperCase() === 'YES'){
@@ -40,6 +44,8 @@ var userGuess;
   else {
     alert('Maybe you should consider singing in the shower!');
   }
+}
+
 
   if (counter >= 2) {
     document.write(user + ', congratulations, you got ' + counter + ' correct answers!');
@@ -51,20 +57,27 @@ var userGuess;
     document.write(user + ', sorry, you got ' + counter + ' correct answers.');
   }
 
-userGuess = prompt (user + ', I hope you are up for a number guessing game :). I am thinking of a number between 1 and 10, what is it?');
+function random() {
+  userGuess = prompt (user + ', I hope you are up for a number guessing game :). I am thinking of a number between 1 and 10, what is it?');
 
-//Generate a random number between 1 and 10
-randomNumber = Math.floor((Math.random() * 10) + 1);
+  //Generate a random number between 1 and 10
+  randomNumber = Math.floor((Math.random() * 10) + 1);
 
-while (userGuess != randomNumber)  {
-  if (userGuess > randomNumber) {
-    userGuess = prompt ('Your number is too high.Keep guessing!');
-    counterNo++;
-  }
-  else if (userGuess <randomNumber) {
-    userGuess = prompt ('Your number is too low.Keep guessing!');
-    counterNo++;
+  while (userGuess != randomNumber)  {
+    if (userGuess > randomNumber) {
+      userGuess = prompt ('Your number is too high.Keep guessing!');
+      counterNo++;
+    }
+    else if (userGuess <randomNumber) {
+      userGuess = prompt ('Your number is too low.Keep guessing!');
+      counterNo++;
+    }
   }
 }
+//calling question functions
+q1();
+q2();
+q3();
+random();
 
-document.write('<p>'+ user + ' ,you guessed in ' + counterNo + ' tries. The correct number is' +randomNumber + '</p>');
+document.write('<p>'+ user + ', you guessed in ' + counterNo + ' tries. The correct number is ' +randomNumber + '.</p>');
